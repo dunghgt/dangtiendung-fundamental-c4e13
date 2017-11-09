fdf476f9ffe6cf4156457eef2b32bab62b02db56
Dang Dung <dangtothedung@gmail.com>
2017-11-10 00:09:04 +0700

    sthg commit

--- session04/homework/study_1.py
+++ session04/homework/study_1.py
@@ -6,8 +6,12 @@ letter_count = {}
 for letter in n:
 
     if letter != " ":
-        letter_count[letter] = letter_count.get(letter, 0) + 1
-    
+        # letter_count[letter] = letter_count.get(letter, 0) + 1
+        if letter not in letter_count:
+            letter_count[letter] = 0
+        else:
+            letter_count[letter] += 1
+
 letter_items = list(letter_count.items())
 
 letter_items.sort()
--- session04/homework/test.py
+++ session04/homework/test.py
@@ -1,3 +1,6 @@
-n = "Hu hu Hu"
-n = n.lower()
-print(n)
+n = "Hu huhihihi2131""'`2312e)(*(*&))"
+
+for letter in "!()_-:;,.?*0123456789\"\`\'":
+    n = n.replace(letter, ' ')
+
+print (n)
